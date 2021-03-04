cc9e913d60cce130286eb4883062711f4a925e65
jobs2306 <joeebaena@gmail.com>
2021-03-04 00:08:18 -0500

    numeror

---
+++ src/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+#include "esp_system.h"
+#include "math.h"
+
+#define TAG "DICE"
+int dice_role()
+{​​
+    int random = esp_random();
+    int positiveNumber = abs(random);
+    printf("%d \n", positiveNumber);
+    int diceNumber = ( positiveNumber % 6) + 1;
+    return diceNumber;
+}​​
+
+void app_main(void)
+{​​
+    while(1)
+    {​​
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        ESP_LOGI(TAG, "random number %d", dice_role());
+    }​​
+}​​
